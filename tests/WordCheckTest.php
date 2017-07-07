1a8cb3504303fd4164cae9fa2e60e8868e563777
Larry <larryjamestaylor@gmail.com>
2017-07-07 11:11:38 -0700

    pass test for numbers

--- tests/WordCheckTest.php
+++ tests/WordCheckTest.php
@@ -8,7 +8,7 @@
             // Arrange
             $test_counter = new Counter;
             $input_string = "I am what I am.";
-            $input_count = "I";
+            $input_count = "i";
 
             // Act
             $result = $test_counter->countWords($input_string, $input_count);
@@ -21,12 +21,25 @@
             // Arrange
             $test_counter = new Counter;
             $input_string = "People are people and people people places.";
-            $input_count = "People";
+            $input_count = "people";
 
             // Act
             $result = $test_counter->countWords($input_string, $input_count);
             // Assert
             $this->assertEquals(4, $result);
+        }
+
+        function testCountWordsNumbers()
+        {
+            // Arrange
+            $test_counter = new Counter;
+            $input_string = "10 out of 10 chimps agree: Jane Goodall is a hack.";
+            $input_count = "10";
+
+            // Act
+            $result = $test_counter->countWords($input_string, $input_count);
+            // Assert
+            $this->assertEquals(2, $result);
         }
     }
 ?>
