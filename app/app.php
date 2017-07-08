31bd67fda9ff6002c2669d4dacd9033b6dedbf64
Larry <larryjamestaylor@gmail.com>
2017-07-07 23:32:20 -0700

    build template for main page

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'/../src/Anagram.php';
+    require_once __DIR__.'/../src/WordCheck.php';
 
     $app = new Silex\Application();
 
