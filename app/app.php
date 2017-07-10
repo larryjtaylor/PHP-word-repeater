f3e85ed582b4001ab6dd645bede31db02418ca07
Larry <larryjamestaylor@gmail.com>
2017-07-10 00:37:45 -0700

    remove static functions to focus on getting display to work right

--- app/app.php
+++ app/app.php
@@ -3,18 +3,24 @@
     require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/WordCheck.php';
 
+
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-    $app->get('/', function() use ($app) {
+    $app->get('/', function() use($app) {
         return $app['twig']->render('words.html.twig');
     });
 
-    $app->get('/view_repeats', function() use ($app){
-        var_dump($count);
-        return $app['twig']->render('views_words.html.twig');
+    $app->get('/view_repeats', function() use($app) {
+        $word = $_GET['word'];
+        $phrase = $_GET['string'];
+        $new_repeat_counter = new RepeatCounter($word, $sentence);
+        $count = $new_repeat_counter->countRepeats($word, $sentence);
+
+        var_dump($word);
+        return $app['twig']->render('view_words.html.twig', array('show_count' => $count, 'show_word' => _GET['word'], 'show_string' => _GET['string']));
     });
-    
+
     return $app;
 ?>
--- src/WordCheck.php
+++ src/WordCheck.php
@@ -3,47 +3,17 @@
     class RepeatCounter
     {
 
-        private $input_string;
-        private $input_count;
-
-        function __composer($input_string, $input_count)
-        {
-            $this->input_string = $input_string;
-            $this->input_count = $input_count;
-        }
-
-        function getString()
-        {
-            return $this->input_string;
-        }
-
-        function setString($input_string)
-        {
-            $this->input_string = (string)$input_string;
-        }
-
-        function getword()
-        {
-            return $this->input_count;
-        }
-
-        function setWord($input_count)
-        {
-            $this->input_count = (string)$input_count;
-        }
-
-
-        function countRepeats ($input_string, $input_count)
+        function countRepeats ($input_string, $input_word)
         {
 
         $count = 0;
-        $phrase_arr = explode(' ', $input_string);
+        $phrase_arr = explode(' ', $string);
         $phrase_arr_lower = array_map('strtolower', $phrase_arr);
-        $input_count_lower = strtolower($input_count);
+        $word_lower = strtolower($word);
 
-            foreach($phrase_arr_lower as $word)
+            foreach($phrase_arr_lower as $item)
             {
-                if($word == $input_count_lower)
+                if($item == $word_lower)
                 {
                     $count ++;
                 }
